a5fdaebc310367b10251542666696c2882aeb350
Олег Волошин <olegvolosin922@gmail.com>
2021-10-18 22:52:07 +0300

    second test

---
+++ UnitTest1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../lab5.1/lab5.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = k(0, 1);
+			Assert::AreEqual(t, cos(1.));
+		}
+	};
+}
